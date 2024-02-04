06c9ef5b4f6da0608323dc114812815298791b83
AbhishekSaroch123 <abhishek@prodsmiths>
2024-02-04 17:46:38 +0530

    Updated Models

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from home import views
 urlpatterns = [
     path('/', views.home,name="home"),
+    path('/blog', views.blog,name="blog"),
+    path('/success_page', views.success_page,name="blog"),
+    path('/conntact', views.contact,name="blog"),
+    path('/about', views.about,name="blog"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render,HttpResponse
 
 def home(request):
-    return render(request,'index.html')
+    people=[{
+        "name":"Abhijeet",
+        "age":23
+    },{
+        "name":"Rohan",
+        "age":24
+    },{
+        "name":"Deep",
+        "age":25
+    }]
+    vegetables=['One','Two']
+    return render(request,"home/index.html",context={'people':people,'text':vegetables})
+
+
+def blog(request):
+    return HttpResponse("<h1>This is Blogs Page!</h1>")
+
+def success_page(request):
+    return HttpResponse("<h1>Success Page</h1>")
+
+def about(request):
+    return render(request,"home/about.html")
+
+def contact(request):
+    return render(request,"home/contact.html")
